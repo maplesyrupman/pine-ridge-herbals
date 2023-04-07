a687397715b209f27e00048b56b7eeb642109ecd
maplesyrupman <maplesyrupman@protonmail.com>
2023-04-07 17:39:47 -0400

    fixed test page

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,17 +1,8 @@
-const aboutSections = [
-  {
-    heading: 'Restaurant Job',
-    paragraphs: [
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe nemo architecto quas fuga velit optio voluptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.',
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe nemo architecto quas fuga velit optio voluptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.',
-      'Lorem ipsum dolor, sit amet consectetur ptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.'
-    ]
-  }, {
-    heading: 'UCF Bootcamp',
-    paragraphs: [
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe nemo architecto quas fuga velit optio voluptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.',
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe nemo architecto quas fuga velit optio voluptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.',
-      'Lorem ipsum dolor, sit amet consectetur ptatibus excepturi iusto rerum, numquam deserunt similique a? Doloremque unde, velit excepturi quas delectus magnam.'
-    ]
-  }
-]
+export default function Test() {
+
+  return (
+    <div>
+      Test
+    </div>
+  )
+}
